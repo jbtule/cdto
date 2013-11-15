282f9033f799f26843e2e39fa7850f0c53e9c24b
jbtule <jay+code@tuley.name>
2013-11-14 20:07:51 -0600

    Switched iterm to workspace invocation

--- plugins/iterm/CD2ITerm.m
+++ plugins/iterm/CD2ITerm.m
@@ -7,46 +7,13 @@
 //
 
 #import "CD2ITerm.h"
-#import "iTerm.h"
 
 @implementation CD2ITerm
 -(BOOL)openTermWindowForPath:(NSString*)aPath{
 	@try{
-		NSMutableString* fixQuotedPath = [[aPath mutableCopy]autorelease];
-		
-		NSString* tCommandSequence =@"clear; cd '%@'";
-		
-		[fixQuotedPath replaceOccurrencesOfString:@"'" withString:@"\"'\"" options:NSCaseInsensitiveSearch range:NSMakeRange(0,[fixQuotedPath length])];
+        
+        [[NSWorkspace sharedWorkspace] openFile:[aPath stringByExpandingTildeInPath] withApplication:@"iTerm.app"];
 
-		iTermITermApplication* iterm = [SBApplication applicationWithBundleIdentifier:@"com.googlecode.iTerm2"];
-		[iterm activate];
-
-		bool isDefaultWindow =NO;
-		iTermTerminal* terminal=nil;
-		if([[iterm windows] count] ==1 ){
-			NSString* windowContents =iterm.currentTerminal.currentSession.contents;
-
-				int originalLength = [windowContents length];
-				
-				NSMutableString *mutableContents = [[windowContents mutableCopy] autorelease];
-				NSMutableString *mutableContents2 = [[windowContents mutableCopy] autorelease];
-				[mutableContents replaceOccurrencesOfString:@"$" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0,[mutableContents length])];
-				[mutableContents2 replaceOccurrencesOfString:@"Last login" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0,[mutableContents2 length])];
-				
-				if([mutableContents length]+1 == originalLength && [mutableContents2 length] < originalLength ){
-					isDefaultWindow =YES;
-					terminal = iterm.currentTerminal;
-				}
-			}
-		
-		if(!isDefaultWindow){
-			terminal =[[[[iterm classForScriptingClass:@"terminal"] alloc] init]autorelease];
-			[iterm.terminals addObject: terminal];
-			[terminal launchSession:@"Default Session"] ;
-		}
-		[terminal.currentSession writeContentsOfFile:nil text:[NSString stringWithFormat:tCommandSequence,fixQuotedPath,nil]];
-
-		
 
 	}@catch(id test){
 		return NO;
